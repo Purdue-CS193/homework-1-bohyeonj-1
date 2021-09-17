2ae5ba52aba29a423059701b5fbe315c74dae86b
bohyeonj <89669608+bohyeonj@users.noreply.github.com>
2021-09-17 19:39:13 -0400

    Update answers.sh

--- answers.sh
+++ answers.sh
@@ -4,42 +4,44 @@
 cd ORIGIN
 
 # Task 1 
-
+mv /Hw2.txt /Hw1.txt
 
 # Task 2
-
+rm -rf ~/Memes/SpongeBob/money.jpeg
 
 # Task 3
-
+mv ~/Memes/StarWars/fafsa.jpg ~/Memes/SpongeBob/fafsa.jpg
 
 # Task 4
-
+rm -r ~/Homework/CS191
 
 # Task 5
-
+mkdir ~/Program/C++
 
 # Task 6
-
+mv ~/Program/Age.cpp ~/Program/C++/Age.cpp
 
 # Task 7
-
+cp ~/Program/Java ~/Program/Project_1  ~Homework/CS180
 
 # Task 8
-
+mv ~/Programs/Count.c ~/Programs/Goodbye.c ~/Programs/C
 
 # Task 9
-
+mv ~/Programs/Name.py ~/Programs/Space.py ~/Programs/Python3
 
 # Task 10
-
+touch ~/Homework/POL141/Essay.txt
 
 # Task 11
-
+ls
 
 # Task 12
 : '
 Type your vim/nano customizations below:
-
+syntax on
+set ttyfast 
+set lazyredraw
 
 
 '
